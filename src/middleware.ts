33e828610f0c5c9906e9a30858059a95bd373236
cryptoMavrik <xdaegonx@gmail.com>
2022-08-01 20:50:36 -0400

    create middle ware to pass cookie

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+
+
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+import { nanoid } from "nanoid";
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+    if (req.cookies.get("poll-token")) return;
+
+    const random = nanoid();
+
+    // Redirect (to apply cookie)
+    const res = NextResponse.redirect(req.nextUrl);
+
+    res.cookies.set("poll-token", random, { sameSite: "strict" });
+
+    return res;
+}
