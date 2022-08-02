6107e50c9a7000e374c346155f69e2630a83a818
cryptoMavrik <xdaegonx@gmail.com>
2022-08-01 21:14:49 -0400

    remove shadow

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -7,7 +7,6 @@ import { z } from "zod";
  */
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
-  SHADOW_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
